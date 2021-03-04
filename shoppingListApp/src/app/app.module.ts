5ff20abb7f8bc73d5e2038ae7ddd95448e857cb5
winderbueno <kevin.gellenoncourt@gmail.com>
2021-03-04 12:37:14 +0100

    Using ngModule to order the app (share the toolbar)

--- shoppingListApp/src/app/app.module.ts
+++ shoppingListApp/src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
-import { ToolbarComponent } from './component/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
