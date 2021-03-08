7b40f609e028536cececa788832b5121a7251887
winderbueno <kevin.gellenoncourt@gmail.com>
2021-03-08 23:16:18 +0100

    working on shopping list component

--- shoppingListApp/src/app/shopping-list/component/shopping-list/shopping-list.component.ts
+++ shoppingListApp/src/app/shopping-list/component/shopping-list/shopping-list.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from 'src/app/_shared/model/product.model';
+
+// Data Model
 import { ShoppingList } from 'src/app/_shared/model/shopping-list.model';
+
+// Mock
+import { PRODUCTS } from 'src/app/_shared/mock/product.mock';
+import { Product } from 'src/app/_shared/model/product.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -8,25 +13,22 @@ import { ShoppingList } from 'src/app/_shared/model/shopping-list.model';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
-
-  product: Product = {
-      id: 1,
-      category: 'testCat',
-      subCategory: 'testSubCat',
-      name: 'nameProd',
-      quantity: 1,
-      isBought: false
-  }
   
   shoppingList: ShoppingList = {
     id: 1,
     idModelShoppingList: 1,
-    productList:[this.product]
+    productList:PRODUCTS
   };
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // Swap value of isBought info for product
+  onClick(prod: Product): void {
+    let isBought = this.shoppingList.productList[prod.id].isBought;
+    isBought ? isBought=false : isBought=true; 
   }
 
 }
