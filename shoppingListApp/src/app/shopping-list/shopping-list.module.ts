3555aafb1cb97d2484d1d37b82c8f153eb3aeab8
winderbueno <kevin.gellenoncourt@gmail.com>
2021-03-08 17:45:01 +0100

    creating shoppingList component + apply different case for file and folder names

--- shoppingListApp/src/app/shopping-list/shopping-list.module.ts
+++ shoppingListApp/src/app/shopping-list/shopping-list.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ShoppingListComponent } from './component/shopping-list/shopping-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ShoppingListComponent],
   imports: [
     CommonModule
   ]
