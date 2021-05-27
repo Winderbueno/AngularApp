0ba91ae66b0c91e475d4a5d6f75e2856822cdc7e
winderbueno <kevin.gellenoncourt@gmail.com>
2021-05-27 23:47:11 +0200

    Std Button to Material

--- shoppingList_Front/src/app/_shared/module/material.module.ts
+++ shoppingList_Front/src/app/_shared/module/material.module.ts
@@ -4,12 +4,13 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 //#endregion
 
 //#region Material Module
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatExpansionModule } from '@angular/material/expansion';
 //#endregion
 
 @NgModule({
@@ -18,12 +19,13 @@ import { MatExpansionModule } from '@angular/material/expansion';
     ReactiveFormsModule
   ],
   exports: [
+    MatButtonModule,
+    MatDialogModule,
+    MatExpansionModule,
     MatIconModule,
     MatInputModule,
     MatSelectModule,
-    MatDialogModule,
-    MatSnackBarModule,
-    MatExpansionModule
+    MatSnackBarModule
   ],
 })
 
