b0cb95dd0843032331ac4228a4193c986bade478
Kevin Gellenoncourt <kevin.gellenoncourt@docaposte.fr>
2021-08-22 10:29:51 +0200

    App_Alert_minorImprove

--- shoppingList_Front/src/app/_store/effect/technical/alert.effects.ts
+++ shoppingList_Front/src/app/_store/effect/technical/alert.effects.ts
@@ -63,9 +63,9 @@ export class AlertEffects {
     this.actions$.pipe(
       ofType(RouterActions.routerRequestAction),
       withLatestFrom(this.store.select(AlertSelectors.getAlertState)),
-      map((actionAndStore) => {
+      map(([action, alertState]) => {
 
-        if(actionAndStore[1].keepAfterRouteChange===true){
+        if(alertState.keepAfterRouteChange===true){
           return AlertActions.hasBeenKeptAfterRouteChange();
         } else { // TODO - Don't dismiss Alert if no alert in the state, but require 'not to dispatch in this case action'
           return AlertActions.dismissAlert();
