4c79d3c6a698c5f7ee5adc34c7455c67c5ca4e82
Kevin Gellenoncourt <kevin.gellenoncourt@docaposte.fr>
2021-10-10 17:28:03 +0200

    Adding TODO

--- shoppingList_Front/src/_module/alert/store/alert.actions.ts
+++ shoppingList_Front/src/_module/alert/store/alert.actions.ts
@@ -5,7 +5,7 @@ import { ActionSourceEnum } from '@action/enum/action-source.enum';
 //#endregion
 
 //#region Model
-import { ModuleEnum } from '@app/model/enum/module.enum';
+import { ModuleEnum } from '@app/model/enum/module.enum'; // TODO - there should not be a dependance on APP, this is true for every actions definition in modules
 import { AlertTypeEnum } from '../model/enum/alert-type.enum';
 //#endregion
 
