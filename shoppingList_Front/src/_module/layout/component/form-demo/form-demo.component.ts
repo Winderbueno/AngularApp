37f0d87bf39c1272425b8b56ce8ba78b01e3e783
winderbueno <kevin.gellenoncourt@gmail.com>
2022-01-10 11:51:19 +0100

    @demo_Form_AliasImportAdaptation

--- shoppingList_Front/src/_module/layout/component/form-demo/form-demo.component.ts
+++ shoppingList_Front/src/_module/layout/component/form-demo/form-demo.component.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 //#endregion
 
 //#region Module
-import { FormComponent } from '@module/ngrx-form/component/form.component';
+import { FormComponent } from '@formNew/component/form.component';
 import { Enum } from '@enum/model/enum.model';
 import * as fromEnum from '@enum/store/';
 import * as fromAlert from '@alert/store/';
