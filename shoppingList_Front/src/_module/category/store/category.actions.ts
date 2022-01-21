916e9b0dc7a9ee7bc163326cd15d7dde71e05265
winderbueno <kevin.gellenoncourt@gmail.com>
2022-01-21 20:13:57 +0100

    @category_cleaning

--- shoppingList_Front/src/_module/category/store/category.actions.ts
+++ shoppingList_Front/src/_module/category/store/category.actions.ts
@@ -1,12 +1,12 @@
 //#region NgRx, Action Creator
 import { props } from '@ngrx/store';
 import { createAction } from '@action/creator/action-creator';
-import { EmitterTypeEnum } from '@module/action/enum/emitter-type.enum';
+import { ModuleEnum } from '@action/enum/module.enum';
+import { EmitterTypeEnum } from '@action/enum/emitter-type.enum';
 //#endregion
 
 //#region Model
-import { ModuleEnum } from '@module/action/enum/module.enum';
-import { Category } from '@module/categorisation/model/category.model';
+import { Category } from '../../model/category.model';
 //#endregion
 
 export const loadCategoryAction = createAction(
