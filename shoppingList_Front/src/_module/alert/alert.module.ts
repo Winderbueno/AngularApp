6b5ab65c79f9892560b04c6954460a9d2e1c2aca
winderbueno <kevin.gellenoncourt@gmail.com>
2022-01-27 17:55:59 +0100

    @alert_ApplyNewLayoutSolution

--- shoppingList_Front/src/_module/alert/alert.module.ts
+++ shoppingList_Front/src/_module/alert/alert.module.ts
@@ -6,6 +6,7 @@ import { EffectsModule } from '@ngrx/effects';
 //#endregion
 
 //#region Module
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '@material/material.module';
 //#endregion
 
@@ -30,6 +31,7 @@ import {
     CommonModule,
 
     /* Module */
+    FlexLayoutModule,
     MaterialModule,
 
     /* Store */
