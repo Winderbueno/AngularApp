e7db1f107cf1d063a6eaceaebc4c5635742929b5
winderbueno <kevin.gellenoncourt@gmail.com>
2022-02-24 18:00:18 +0100

    @shoppingList_WIP

--- shoppingList_Front/src/app/shopping-list/store/shopping-list.reducers.ts
+++ shoppingList_Front/src/app/shopping-list/store/shopping-list.reducers.ts
@@ -7,6 +7,8 @@ import { ShoppingListState, initialState, adapter } from './shopping-list.state'
 import * as fromAPI from '../service/shopping-list.api.actions';
 import * as fromComponent from '../component';
 import * as AccountAPIActions from '@account/service/account.api.actions'; // TODO
+import { Update } from '@ngrx/entity';
+import { ShoppingList } from '../model/current/shopping-list.model';
 //#endregion
 
 export const featureKey = 'shoppingList';
@@ -28,7 +30,42 @@ const shoppingListReducer = createReducer(
   ),
 
   on(fromComponent.productChipClickedAction,
-    (state, action) => { return state }
+    (state, action) => {
+
+      let changes = {
+        ...state.entities[action.shoppingListId],
+        catProducts: state.entities[action.shoppingListId]?.catProducts?.map(
+          (item, index) => {
+            if (index !== 0) { return item; } // Not the one we want to change
+            return { ...item,
+              subCatProducts: item.subCatProducts.map(
+                (item, index) => {
+                  if (index !== 0) { return item; }
+                  return { ...item,
+                    products: item.products.map(
+                      (item, index) => {
+                        if (index !== (action.productUpdate.id as number -1)) { return item; }
+                        return { ...item,
+                          bought: !item.bought
+                        };
+                      }
+                    )
+                  };
+                }
+              )
+            }
+          }
+        )
+      }
+
+
+      let shoppingListUpdt: Update<ShoppingList> = {
+        id: action.shoppingListId,
+        changes: changes
+      };
+      
+      return adapter.updateOne(shoppingListUpdt, state);
+    }
   ),
 );
 
