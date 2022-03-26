6b2d3911a074e525f4933d60694ae088ca8daffd
winderbueno <kevin.gellenoncourt@gmail.com>
2022-03-26 22:08:55 +0100

    @core_ManageMobileFooterVisibility

--- shoppingList_Front/src/app/_core/page/home/home.page.ts
+++ shoppingList_Front/src/app/_core/page/home/home.page.ts
@@ -1,5 +1,6 @@
 //#region Angular, Material, NgRx
 import { Component, HostListener } from '@angular/core';
+import { MediaObserver } from '@angular/flex-layout';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 //#endregion
@@ -17,8 +18,13 @@ export class HomePage {
 
   readonly isOpenSideNav$: Observable<boolean> = this.store.select(fromStore.isOpenSideNav);
   footerHideXs = false;
+  isKeyboardVisible = false;
+  windowHeight = window.innerHeight;
 
-  constructor(private store: Store) { }
+  constructor(
+    private store: Store,
+    private readonly mediaObserver: MediaObserver
+  ) {}
 
   @HostListener('window:storage', ['$event'])
   onStorage(event: any): void {
@@ -26,21 +32,25 @@ export class HomePage {
       this.store.dispatch(fromStore.accountWindowStorageChangeAction({ event: event }));
   }
 
-  @HostListener('focusin', ['$event'])
-  onFocus(event: any): void {
-    if (event.srcElement.localName === 'input') { this.footerHideXs = true; }
-  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    // On small screen, 
+    //  -> 'hide/show' footer when windowHeight is 'shortened/extended'
+    //  <=> which realistically means that a virtual keyboard where shown/hidden
+    // Notable Exception :
+    //   - If browser is in a pop-up view and resize is done manually
+    //   - In chrome browser with auto-hide feature of chrome searchbar
+    // Though unperfect, this solution is acceptable 
+    if(this.mediaObserver.isActive('xs')){
+      (event.currentTarget.innerHeight < this.windowHeight) ?
+        this.isKeyboardVisible = true :
+        this.isKeyboardVisible = false;
 
-  @HostListener('focusout', ['$event'])
-  onFocusOut(event: any): void {
-    if (event.srcElement.localName === 'input') { this.footerHideXs = false; }
-  }
-
-  @HostListener('keydown', ['$event'])
-  onKeyDown(event: any): void {
-    // On small screen, if user press return, show footer
-    if (event.keyCode === 13) { this.footerHideXs = false; }
-  }
+      this.isKeyboardVisible ? 
+        this.footerHideXs = true :
+        this.footerHideXs = false;
+    }
+  }  
 
   closeSideNav() {
     this.store.dispatch(fromStore.closeSideNavAction());
