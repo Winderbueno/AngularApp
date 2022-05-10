f6f86c60219d216e61955fa76c6c646b58b60b16
winderbueno <kevin.gellenoncourt@gmail.com>
2022-05-10 23:21:36 +0200

    @app_AdaptTo_@buttonWithNgContent

--- shoppingList_Front/src/app/_demo/component/loader/loader.component.ts
+++ shoppingList_Front/src/app/_demo/component/loader/loader.component.ts
@@ -1,6 +1,5 @@
 //#region Angular, Material, NgRx
 import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
 //#endregion
 
 //#region Module
@@ -12,15 +11,7 @@ import * as fromLoader from '@loader/store';
   selector: 'demo-loader',
   templateUrl: './loader.component.html'
 })
-export class LoaderComponent {  
-
-  constructor(public store: Store) {}
-
-  startLoader() { 
-    this.store.dispatch(fromLoader.startLoaderAction({triggerSource : '' }));
-  }
-
-  stopLoader() { 
-    this.store.dispatch(fromLoader.stopLoaderAction()); 
-  }  
+export class LoaderComponent {
+  startLoaderAction = fromLoader.startLoaderAction({triggerSource : '' });
+  stopLoaderAction = fromLoader.stopLoaderAction();
 }
