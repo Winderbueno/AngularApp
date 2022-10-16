643b9b7ac73fcfaf22a1e1ce4dd5abeb4b296a9d
winderbueno <kevin.gellenoncourt@gmail.com>
2022-10-16 22:25:20 +0200

    @entreprise_ImplVersemLibe(WIP)

--- shoppingList_Front/src/app/enterprise/component/income-tax/income-tax.component.ts
+++ shoppingList_Front/src/app/enterprise/component/income-tax/income-tax.component.ts
@@ -3,6 +3,10 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 //#endregion
 
+//#region Module
+import * as fromForm from '@form/store';
+//#endregion
+
 //#region This
 import { IncomeTaxRow } from '../../model/income-tax-row.model';
 import * as fromStore from '../../store';
@@ -18,9 +22,16 @@ export class IncomeTaxComponent {
   
   dataSource: IncomeTaxRow[] = [];
   displayedColumns: string[] = ['range', 'rate', 'amount'];
+
+  isVersementLiberatoire = false;
   
   constructor(private store: Store) {
-    this.store.select(fromStore.selectIncomeTaxDataSource)
+    this.store.select(fromStore
+      .selectIncomeTaxDataSource)
       .subscribe(ds => this.dataSource = ds);
+
+    this.store.select(fromForm
+      .selectControlValue('Income', 'VersementLiberatoire'))
+      .subscribe(val => this.isVersementLiberatoire = val as boolean)
   }
 }
--- shoppingList_Front/src/app/enterprise/effect/income-tax.effects.ts
+++ shoppingList_Front/src/app/enterprise/effect/income-tax.effects.ts
@@ -7,7 +7,8 @@ import { filter, withLatestFrom, switchMap } from 'rxjs/operators';
 //#endregion
 
 //#region Module
-import * as fromForm from '@form/model';
+import { FormValue } from '@form/model';
+import * as fromForm from '@form/store';
 import { SetValueAction } from 'ngrx-forms';
 import { FormatService } from '../service/format.service';
 //#endregion
@@ -24,34 +25,44 @@ export class IncomeTaxEffects {
   computeIncomeTax$ = createEffect(() =>
     this.actions$.pipe(
       ofType(SetValueAction.TYPE),
-      filter((action: SetValueAction<fromForm.FormValue>) => action.controlId === 'Income.CA'),
+      filter((action: SetValueAction<FormValue>) => 
+        (action.controlId === 'Income.CA' || action.controlId === 'Income.VersementLiberatoire')),
       switchMap((action) =>
         of(action).pipe(
           withLatestFrom(this.store.select(fromStore.selectState)),
-          switchMap(([action, state]) => {
+          withLatestFrom(this.store.select(fromForm.selectFormValue('Income'))),
+          switchMap(([[, state], formValue]) => {
 
-            // Apply tax allowance on CA
-            let CA = action.value as unknown as number;
-            let CA_Abattu = CA * (1 - 0.34);
-
-            // Compute income tax amount by slices
+            let isVersLib = formValue.VersementLiberatoire as boolean;
+            let CA = formValue.CA as number;
             let ds: IncomeTaxRow[] = [...state.incomeTaxDataSource.map(row => { return { ...row }; })];
             let totalAmount: number = 0;
-            ds.forEach((row, i) => {
-              row.amount = 0;
-              if (CA_Abattu > state.thresholds[i + 1]) {
-                row.amount = this.format.ToDecimal((state.thresholds[i + 1] - state.thresholds[i]) * row.rate! / 100);
-              }
-              else if (CA_Abattu > state.thresholds[i]) {
-                row.amount = this.format.ToDecimal((CA_Abattu - state.thresholds[i]) * row.rate! / 100);
-              }
-              totalAmount = row.amount + totalAmount;
-            });
+
+            if(isVersLib){
+              // TODO - 2,2 is only for BNC (rate is different for BIC..)
+              totalAmount = this.format.ToDecimal(CA * 2.2 / 100);
+              ds = [{ range: 'Total' }];
+            } else {
+              // Apply tax allowance on CA
+              let CA_Abattu = CA * (1 - 0.34);
+
+              // Compute by tax slices
+              ds.forEach((row, i) => {
+                row.amount = 0;
+                if (CA_Abattu > state.thresholds[i + 1]) {
+                  row.amount = this.format.ToDecimal((state.thresholds[i + 1] - state.thresholds[i]) * row.rate! / 100);
+                }
+                else if (CA_Abattu > state.thresholds[i]) {
+                  row.amount = this.format.ToDecimal((CA_Abattu - state.thresholds[i]) * row.rate! / 100);
+                }
+                totalAmount = row.amount + totalAmount;
+              });
+            }
 
             // Set total row
             let totalRow = ds[ds.length - 1];
             totalRow.amount = this.format.ToDecimal(totalAmount);
-            totalRow.rate = Math.trunc(this.format.ToDecimal(totalAmount * 100 / CA));
+            totalRow.rate = Math.round(this.format.ToDecimal(totalAmount * 100 / CA));
 
             return of(fromStore.incomeTaxComputedAction({dataSource: ds }));
           })
